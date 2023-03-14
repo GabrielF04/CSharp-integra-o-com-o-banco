4d798825ae251bd647699a846a6f65cc4ae96425
GabrielF04 <122302554+GabrielF04@users.noreply.github.com>
2023-03-14 15:39:57 -0300

    Add files via upload

---
+++ Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required(ErrorMessage = "Esse campo é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "Esse campo é obrigatorio")]
+        [MaxLength(50)]
+        public string Genero { get; set; }
+        [Required(ErrorMessage = "Esse campo é obrigatorio")]
+        [Range(70, 600, ErrorMessage = "A duração deve ter de 70 a 600 min")]
+        public int Duracao { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ FilmeController.cs
@@ -0,0 +1,42 @@
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+
+[ApiController]
+[Route("Controller")]
+public class FilmeController : ControllerBase
+{
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public CreatedAtActionResult AdicionaFilme([FromBody]Filme filme)   
+    {
+      _context.Filmes.Add(filme);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
+    }
+    [HttpGet]
+   
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50) 
+    {
+        return _context.Filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = _context.Filmes.SingleOrDefault(filme
+            => filme.Id == id);
+        if(filme == null) return NotFound();
+        return Ok(filme);
+    }
+}
